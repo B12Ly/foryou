242a27879f4805cb84d5dc23b4aa49cb16fc0cce
Chabil Satsanaphiban <6710110088@psu.ac.th>
2025-04-28 19:05:38 +0700

    cover jigsaw

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,34 +1,42 @@
 import React, { useEffect, useState } from "react";
 import "./login.css";
-import book from "../assets/book.png";
+import book from "../assets/book2.png";
 import Jigsaw from "./Jigsaw";
-import cover from "../assets/cover.png"; 
-import uncover from "../assets/uncover.png"; 
+import cover from "../assets/cover.png";
+import uncover from "../assets/uncover.png";
 
 function Login() {
   const [isVisible, setIsVisible] = useState(false);
-  const [isCovered, setIsCovered] = useState(true); 
+  const [isCovered, setIsCovered] = useState(true);
+  const [isUncovering, setIsUncovering] = useState(false);
 
   useEffect(() => {
     setTimeout(() => setIsVisible(true), 100);
   }, []);
 
   const handleCoverClick = () => {
-    setIsCovered(false); 
+    setIsUncovering(true);
+    setTimeout(() => {
+      setIsCovered(false);
+    }, 1000);
   };
 
   return (
     <div className={`login ${isVisible ? "fade-in" : ""}`}>
       <div className="book-container">
         <img src={book} alt="centered" className="book" />
-        
+
         {isCovered && (
           <div className="cover-container">
-            <img src={cover} alt="cover" className="cover-image" />
+            <img
+              src={cover}
+              alt="cover"
+              className={`cover-image ${isUncovering ? "fade-out" : ""}`} 
+            />
             <img
               src={uncover}
               alt="uncover"
-              className="uncover-button"
+              className={`uncover-button ${isUncovering ? "fade-out" : ""}`}
               onClick={handleCoverClick}
             />
           </div>
