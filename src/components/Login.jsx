8b0048e231a0ce8f708fb896f7ad1828d61d904b
Chabil Satsanaphiban <6710110088@psu.ac.th>
2025-04-28 18:29:09 +0700

    re-size and cover jigsaw

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,18 +2,38 @@ import React, { useEffect, useState } from "react";
 import "./login.css";
 import book from "../assets/book.png";
 import Jigsaw from "./Jigsaw";
+import cover from "../assets/cover.png"; 
+import uncover from "../assets/uncover.png"; 
 
 function Login() {
   const [isVisible, setIsVisible] = useState(false);
+  const [isCovered, setIsCovered] = useState(true); 
 
   useEffect(() => {
     setTimeout(() => setIsVisible(true), 100);
   }, []);
 
+  const handleCoverClick = () => {
+    setIsCovered(false); 
+  };
+
   return (
     <div className={`login ${isVisible ? "fade-in" : ""}`}>
       <div className="book-container">
         <img src={book} alt="centered" className="book" />
+        
+        {isCovered && (
+          <div className="cover-container">
+            <img src={cover} alt="cover" className="cover-image" />
+            <img
+              src={uncover}
+              alt="uncover"
+              className="uncover-button"
+              onClick={handleCoverClick}
+            />
+          </div>
+        )}
+
         <div className="jigsaw-wrapper">
           <Jigsaw />
         </div>
